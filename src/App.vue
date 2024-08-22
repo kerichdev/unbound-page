651a6320fcb1ccb63facfcc9ca03e3dc3d3ee10b
kerichdev <kerichuudev@gmail.com>
2024-08-22 23:04:29 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+</script>
+
+<template>
+  <div class="contentRow">
+    <div class="contentColumn">
+      <img src="./assets/unboundlogo.png" class="unboundLogo">
+      <div class="reserveView">
+        <div class="fancyTag"><p class="fancyText">@kerichuu</p></div>
+        <p class="congrats">All done, kerichuu!</p>
+        <p class="infoText">Not a fan of the username you picked? Or maybe you no longer want to sign up? Just <a href="https://cancel.my.reservation" style="text-decoration: underline; color: #6e7bc1;">cancel your reservation</a>.</p>
+      </div>
+      <div class="madeBy"><p>Made by @kerichuu</p></div>
+    </div>
+    <div class="contentColumn"> <img class="spiral" src="./assets/spiral.png"> </div>
+  </div>
+</template>
